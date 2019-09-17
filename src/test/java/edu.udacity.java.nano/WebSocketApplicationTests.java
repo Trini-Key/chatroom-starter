eb91222394f34bb869f05143333c1f33bf16b5b8
Key <KeyJerome@gmail.com>
2019-09-17 06:31:30 -0400

    started adding tests. Two so far for chat and login from the WebSocketChatApplication @RestController.

---
+++ src/test/java/edu.udacity.java.nano/WebSocketApplicationTests.java
@@ -0,0 +1,46 @@
+package edu.udacity.java.nano;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration
+@WebMvcTest(WebSocketChatApplication.class)
+public class WebSocketApplicationTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void ModelAndViewApi() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/login"));
+    }
+
+    @Test
+    public void ModelAndViewChatApi() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/chat")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("/chat"));
+    }
+
+}
+

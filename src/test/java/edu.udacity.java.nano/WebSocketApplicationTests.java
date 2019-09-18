25156c0b6b0d8c1276c2c55b515392b6d0fdda05
Key <KeyJerome@gmail.com>
2019-09-17 20:58:25 -0400

    optimized imports

--- src/test/java/edu.udacity.java.nano/WebSocketApplicationTests.java
+++ src/test/java/edu.udacity.java.nano/WebSocketApplicationTests.java
@@ -14,9 +14,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.io.IOException;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
